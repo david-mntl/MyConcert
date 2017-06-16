d97c873fabde2a72bafe837920b75e3dca1e16e3
abrahamon <ipseabraham@gmail.com>
2017-06-16 17:14:39 -0600

    unit testing .sql

--- DB/UT.sql
+++ DB/UT.sql
@@ -78,12 +78,8 @@ EXEC spAddBillboardPhoto 6, '';
 
 
 --Festivales--
-EXEC spAddFestival 3,"02/02/18","03/03/18", 'Estadio Nacional','This fesitival will be awesome';
-EXEC spAddFestival 4,"03/03/18","04/04/18", 'Tecnológico, Costa Rica','This fesitival will be cool';
-EXEC spAddFestival 5,"04/04/18","05/05/18", 'Parque de Diversiones','This fesitival will be not so awesome';
-EXEC spAddFestival 6,"05/05/18","06/06/18", 'Centro de Eventos Pedregal','This fesitival will be great';
-
-UPDATE FESTIVAL SET FK_ID_Ubication=1;
+EXEC spAddFestival 1,"02/02/18","03/03/18", 'Estadio Nacional','This fesitival will be awesome';
+EXEC spAddFestival 2,"03/03/18","04/04/18", 'Tecnológico, Costa Rica','This fesitival will be cool';
 
 EXEC spAddCategoryToFestival 1,1;
 EXEC spAddBandToFestival 1,1;
@@ -91,45 +87,6 @@ EXEC spAddBandToFestival 1,1;
 EXEC spAddCategoryToBillboard 1,1;
 EXEC spAddBandToBillboard 1,1,1;
 
-
-/*
-INSERT INTO ARTIST (NAME) VALUES ('a')
-INSERT INTO ARTIST (NAME) VALUES ('aa')
-INSERT INTO ARTIST (NAME) VALUES ('aaa')
-INSERT INTO ARTIST (NAME) VALUES ('aaaa')
-INSERT INTO ARTIST (NAME) VALUES ('aaaaa')
-INSERT INTO ARTIST (NAME) VALUES ('aaaaaa')
-INSERT INTO ARTIST (NAME) VALUES ('aaaaaaaa')
-
-
-insert into FESTIVAL (FK_ID_Category_List, FestivalStart, FestivalEnd, FK_ID_Place, FK_ID_EventState) VALUES (1,getDate(),getDate(),1,1)
-insert into FESTIVAL (FK_ID_Category_List, FestivalStart, FestivalEnd, FK_ID_Place, FK_ID_EventState) VALUES (2,getDate(),getDate(),2,2)
-
-insert into FESTIVAL_CATEGORY (FK_ID_Category) VALUES (1)
-insert into FESTIVAL_CATEGORY (FK_ID_Category) VALUES (2)
-insert into FESTIVAL_CATEGORY (FK_ID_Category) VALUES (3)
-
-insert into FESTIVAL_CATEGORY_LIST(FK_ID_Festival, FK_ID_FestivalCategory) VALUES (1,1)
-
-
-insert into FESTIVAL_BANDS_LIST (FK_ID_FestivalCategory, FK_ID_Band) VALUES(2,1)
-insert into FESTIVAL_BANDS_LIST (FK_ID_FestivalCategory, FK_ID_Band) VALUES(2,2)
-insert into FESTIVAL_BANDS_LIST (FK_ID_FestivalCategory, FK_ID_Band) VALUES(2,3)
-insert into FESTIVAL_BANDS_LIST (FK_ID_FestivalCategory, FK_ID_Band) VALUES(2,4)
-insert into FESTIVAL_BANDS_LIST (FK_ID_FestivalCategory, FK_ID_Band) VALUES(1,6)
-insert into FESTIVAL_BANDS_LIST (FK_ID_FestivalCategory, FK_ID_Band) VALUES(1,7)
-insert into FESTIVAL_BANDS_LIST (FK_ID_FestivalCategory, FK_ID_Band) VALUES(1,8)
-insert into FESTIVAL_BANDS_LIST (FK_ID_FestivalCategory, FK_ID_Band) VALUES(1,9)
-insert into FESTIVAL_BANDS_LIST (FK_ID_FestivalCategory, FK_ID_Band) VALUES(1,10)
-
-
-
-
--- 2 categorias y cada una con 3 bandas para 1 festival
-insert into billboard (Name,StartVotingDate,Endvotingdate,FK_ID_Place,FK_ID_EventState) Values ('billN',getDate(),getDate(),2,1)
-insert into festival_category(FK_ID_Category) VALUES (4)
-insert into FESTIVAL_CATEGORY_LIST(FK_ID_Festival, FK_ID_FestivalCategory) VALUES (2,4)
-*/
 
 
 
